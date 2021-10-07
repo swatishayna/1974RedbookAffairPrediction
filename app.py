b691816bd37cf2e8209d99d07442269c8866e608
Swati <swati308mmu@gmail.com>
2021-10-07 12:21:08 +0530

    StreamliUpdated

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ scaler = StandardScaler()
 
 
 
-input_required =['occ_2', 'occ_3', 'occ_4', 'occ_5', 'occ_6', 'occ_husb_2', 'occ_husb_3',
-       'occ_husb_4', 'occ_husb_5', 'occ_husb_6', 'rate_marriage', 'age',
-       'yrs_married', 'children', 'religious', 'educ']
+
 
 
 occ_woman = st.radio('Occupation of Lady',  ('farming/semi-skilled/unskilled', '"white collar', 'teacher/nurse/writer/technician/skilled', 'managerial/business','professional with advanced degree','others'))
 occ_man   = st.radio('Occupation of Man',  ('farming/semi-skilled/unskilled', '"white collar', 'teacher/nurse/writer/technician/skilled', 'managerial/business','professional with advanced degree','others'))
-
+rate_marriage = st.radio('Rating Input of Marriage Life', ('VeryPoor','Poor','Good','VeryGood','Excellent'))
+age = st.slider('Age of lady', min_value = 17, max_value=42)
+yrs_married = st.slider('Number of Married Years', min_value = 0, max_value=23)
+children = st.number_input('ChildrenCount', min_value =0, max_value =5)
+religious = st.radio('Are You religious?',('Not at all', 'Yes but not much', 'Yes', 'Highly Religious'))
+educ= st.radio('level of education', ('grade school','high school', 'some college', 'college graduate','some graduate school','advanced degree'))
